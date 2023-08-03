463290c2b04d8ff317a705321bacfb2c69f24895
Tai Nguyen <nguyenductai.job@gmail.com>
2023-08-03 11:09:37 +0700

    Init xcode file

---
+++ TaiNguyen-Notes/TaiNguyen-Notes/TaiNguyen_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaiNguyen_NotesApp.swift
+//  TaiNguyen-Notes
+//
+//  Created by Tai Nguyen on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaiNguyen_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
